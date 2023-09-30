c7b4c2d04856a2529e2de25a89976b305053c844
Slacky300 <rehman.khan.300a@gmail.com>
2023-09-30 23:32:07 +0530

    changed api key

--- controllers/emergencyCntrl.js
+++ controllers/emergencyCntrl.js
@@ -14,14 +14,17 @@ const getData = async(url) => {
     
     return data
   }catch(e){
-    console.log(e);
+    console.log(e.message);
   }
 }
 
 const sendemergencyCntrl = asyncHandler(async (req, res) => {
   
   const {userId, lat, long} = req.body;
-  const resp = await getData(`https://apis.mapmyindia.com/advancedmaps/v1/e06abc40ab1a2cb7d082646670f051b7/rev_geocode?lat=${lat}&lng=${long}`)
+  if(!lat || !long){
+    res.status(403).json({message: "latitude or longitude is missing"})
+  }
+  const resp = await getData(`https://apis.mapmyindia.com/advancedmaps/v1/efd1bc9e76b7a36cb990af517a48f3c3/rev_geocode?lat=${lat}&lng=${long}`)
   pincode = resp.results[0].pincode;
   formattedAddress = resp.results[0].formatted_address;
   const  user = await User.findById(userId);
