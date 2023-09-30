dd0cfdfb13d2950d3948f5240fdf06d5e3299db2
Slacky300 <rehman.khan.300a@gmail.com>
2023-09-30 14:02:58 +0530

    added incident model

---
+++ models/incidentRptModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const IncidentSchema = mongoose.Schema({
+
+
+    user:{
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    meidaSt:{
+        type:String,
+
+    },
+    pincodeOfIncident:{
+        type: String,
+        required: [true,"Pincode required"]
+    },
+    report:{
+        type: String,
+        required: [true,"Abstract required"]
+    }
+});
+const Incident = mongoose.model('Incident',IncidentSchema);
+module.exports = {Incident}
