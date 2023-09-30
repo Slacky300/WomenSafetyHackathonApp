ed1bcb3a25961b51204ddda337265cbfba7f99a9
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-30 12:06:01 +0530

    user mode completed

--- models/userModel.js
+++ models/userModel.js
@@ -1,48 +1,53 @@
 const mongoose = require("mongoose");
 
-
 const UserSchema = mongoose.Schema(
-    {
-       
-        fname: {
-            type: String,
-            required: true
-            
-        },
-        lname: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            unique: [true, "This email is already in use"],
-        },
-        profile: {
-            type: String
-        },
-        phoneNo: {
-            type: String,
-            unique: [true, "This phone number is already in use"],
-        },
-        gender: {
-            type: String,
-            enum: ["male", "female"],
-        },       
-        isVerified: {
-            type: Boolean,
-            default: false
-        },
-        verificationToken: {
-            type: String,
-            default: null
-        },
-       
+  {
+    uname: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      unique: [true, "This email is already in use"],
+    },
+    profile: {
+      type: String,
+    },
+    phoneNo: {
+      type: String,
+      unique: [true, "This phone number is already in use"],
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      default: null,
+    },
+    city: {
+      type: String,
+    },
+    emergencNo: {
+      type: Number,
+    },
+    emergencyMail: {
+      type: String,
+      unique: [true, "This email is already in use"],
+    },
+    pinCode: {
+      type: Number,
+    },
+    address: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
-
-
-
 
 const User = mongoose.model("User", UserSchema);
 module.exports = { User, UserSchema };
