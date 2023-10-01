df0156edecb9acb77e6aff87ec480ec65632afe3
Slacky300 <rehman.khan.300a@gmail.com>
2023-10-01 10:38:27 +0530

    fixed bug

--- src/pages/IncidentReport.jsx
+++ src/pages/IncidentReport.jsx
@@ -3,27 +3,57 @@ import Sidebar from "../Components/Dash/Sidebar";
 import { useState } from "react";
 import axios from 'axios'
 import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const Dashboard = (props) => {
-  const [incidentreport, setincidentreport] = useState([])
+  const [incidentreport, setincidentreport] = useState([]);
+  const [report,setReport] = useState("")
 
   const getAllIncident = async () => {
     try {
-      const { data } = await axios.get('https://womensecbackend.onrender.com/api/v1/incidents')
+      const res = await fetch('https://womensecbackend.onrender.com/api/v1/incidents',{
+        method: "GET",
+        headers: {"Content-type": "application/json"}
+      })
 
-      if (data) {
+      if(res.status === 200){
+        const data  = await res.json();
+       console.log(data)
         setincidentreport(data)
+      
       }
-      console.log(data)
+
+      
     } catch (err) {
       console.log(err)
+    }
+  }
+
+  const [ack,setAck] = useState(false)
+
+
+  const acknowledge = async (incId) => {
+    try{
+      const res = await fetch(`https://womensecbackend.onrender.com/api/v1/incidents/${incId}`,{
+        method:"PATCH",
+        headers: {'Content-type': 'application/json'}
+      });
+
+      if(res.status === 200){
+        toast.success("Updated Successfully")
+      }
+
+    }catch(e){
+      toast.error("Error while Updating !")
+    }finally{
+      setAck(!ack);
     }
   }
 
   useEffect(() => {
     getAllIncident();
     window.scrollTo(0, 0)
-  }, [])
+  }, [ack])
 
   return (
     <div className="d-flex justify-content-start">
@@ -44,21 +74,61 @@ const Dashboard = (props) => {
               <th scope="col">Report</th>
               <th scope="col">Address</th>
               <th scope="col">Pincode</th>
+              <th scope="col">Incident recorded Date & Time</th>
+              <th scope="col">Acknowledgement Status</th>
+              
             </tr>
           </thead>
-          <tbody className="text-center">
-            {incidentreport.map((p) => (
-              <tr >
-                <th scope="row">{p.uname}</th>
-                <td>{p.report}</td>
-                <td>{p.address}</td>
-                <td>{p.pincode}</td>
-              </tr>
+          <tbody className="text-center ">
+            {incidentreport.map((p,_) => (
+                <>
+                    {p.isSeen?(<>
+                    
+                      <tr  key={_}>
+                      <th  style={{color: "green"}} scope="row">{p.uname} Hello</th>
+                      <td>{p.report}</td>
+                      <td>{p.address}</td>
+                      <td>{p.pincode}</td>
+                      <td>{p.createdAt.split('T')[0]} - {p.createdAt.split('T')[1].split('.')[0]}</td>
+                      <td><button className="btn btn-success" >Acknowledged</button></td>
+                    </tr>
+                    </>):(<>
+                      <tr  key={_}>
+                      <th  style={{color: "red"}} scope="row">{p.uname}</th>
+                      <td><button type="button" class="btn btn-dark" onClick={() => setReport(p.report)} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                       View Report
+                      </button></td>
+                      <td>{p.address}</td>
+                      <td>{p.pincode}</td>
+                      <td>{p.createdAt.split('T')[0]} - {p.createdAt.split('T')[1].split('.')[0]}</td>
+                      <td><button className="btn btn-danger" onClick={() => acknowledge(p._id)} >Acknowledge</button></td>
+                    </tr>
+                    </>)}
+                </>
             ))}
           </tbody>
         </table>
       </div>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              {report}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div >
+
   );
 };
 
